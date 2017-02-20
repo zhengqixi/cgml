0bc28680c6a299b3d989799ee50165efb46432ba
zhengqixi <zhengqixi@github.com>
2017-02-20 17:38:57 -0500

    god bless gonna start training soon

--- Assignment3/mnist_train.py
+++ Assignment3/mnist_train.py
@@ -1,12 +1,24 @@
-from poopoonet import  poopoonet
-import tensorflow as tf
+from poopoonet import poopoonet
 from tensorflow.contrib.learn.python.learn.datasets.mnist import read_data_sets
-
+import json
 mnist = read_data_sets("mnist_data/",  one_hot=True)
 train = mnist[0]
 validate = mnist[1]
 test = mnist[2]
-dev = tf.constant(train.images, dtype=tf.float32, shape=[55000, 28, 28, 1])
-print(dev)
-net = poopoonet.build_model(dev, 10, True)
-print(net)
+
+train_dict = {'images':train.images, 'labels':train.labels}
+validate_dict = {'images':validate.images, 'labels':validate.labels}
+test_dict = {'images':test.images, 'labels':test.labels}
+net = poopoonet()
+net.build_model([28, 28, 1], 10)
+epoch, losses, validation_epoch, validation_accuracy = net.train_model(0.005, train_data=train_dict, validation_data=validate_dict, validation_epoch=5000, outdir="mnist_logdir")
+predictions = net.infer_model(test_dict)
+total_accuracy= net.accuracy(predictions, test.labels)
+
+with open("validation_data.json", "w") as validation_file:
+    json.dump({'epoch':validation_epoch, 'accuracy':validation_accuracy}, validation_file)
+
+with open("loss_data.json", "w") as loss_file:
+    json.dump({'epoch':epoch, 'losses':losses}, loss_file)
+
+print(total_accuracy)
--- Assignment3/poopoonet.py
+++ Assignment3/poopoonet.py
@@ -1,26 +1,79 @@
 import tensorflow.contrib.slim as slim
 import tensorflow as tf
+import numpy as np
+import os
 class poopoonet:
 
-    @staticmethod
-    def build_model(input, num_output, training):
-        with slim.arg_scope([slim.conv2d, slim.fully_connected],
+    def __init__(self):
+        self.sess = tf.Session()
+
+    def build_model(self, input_shape, num_output):
+        #shape: [dimension1, dimension2, channels]
+        #Too hard to do minibatch everything gonna be online cuz that's superior anyway
+        self.inputs = tf.placeholder(tf.float32, shape=[1, input_shape[0], input_shape[1], input_shape[2]], name='input')
+        self.num_output = num_output
+        self.input_shape = input_shape
+        with slim.arg_scope([slim.layers.convolution, slim.layers.fully_connected],
                             weights_initializer=tf.contrib.layers.xavier_initializer(),
                             weights_regularizer=slim.l2_regularizer(0.5)):
-            net = slim.layers.convolution(input, num_outputs=20, kernel_size=3, scope='conv1')
+            net = slim.layers.convolution(self.inputs, num_outputs=20, kernel_size=3, scope='conv1')
             net = slim.layers.max_pool2d(net, 3, scope='pool1')
             net = slim.layers.convolution(net, num_outputs=15, kernel_size= 2, stride=1,scope='conv2')
             net = slim.layers.max_pool2d(net, 2, stride=2, scope='pool2')
             net = slim.layers.flatten(net, scope='flat')
             net = slim.layers.fully_connected(net, 100)
-            net = slim.stack(net, slim.fully_connected, [50, num_output], scope='fc')
-            return net
+            net = slim.layers.fully_connected(net, 50)
+            net = slim.layers.fully_connected(net, num_output, activation_fn=tf.nn.softmax, weights_regularizer=None)
+            self.net = net
 
-    @staticmethod
-    def train_model(train_data, validate_data, outdir, num_output):
-        predictions =  poopoonet.build_model(train_data['images'], num_output, False)
-        slim.losses.softmax_cross_entropy(predictions, train_data['labels'])
-        total_loss = slim.losses.get_total_loss()
-        optimizer = tf.train.GradientDescentOptimizer()
-        trainer = slim.learning.create_train_op(total_loss, optimizer)
-        print(trainer)
+    def train_model(self, learning_rate, train_data, validation_data, validation_epoch, outdir=None):
+        self.predictions = tf.placeholder(tf.float32, shape=[1, self.num_output])
+        predict_loss = tf.losses.softmax_cross_entropy(self.net, self.predictions)
+        weight_loss = tf.reduce_sum(tf.losses.get_regularization_losses())
+        total_loss = predict_loss + weight_loss
+        optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
+        self.trainer = slim.learning.create_train_op(total_loss, optimizer)
+        self.sess.run(tf.global_variables_initializer())
+        batch_image, batch_label = self.get_batch(train_data)
+        loss = 0
+        epoch = 1
+        losses_vector = []
+        epoch_vector = []
+        validation_accuracy = []
+        validation_epoch_time= []
+        for image, label in zip(batch_image, batch_label):
+            _, loss = self.sess.run([self.trainer, total_loss], feed_dict={self.inputs:image, self.predictions:label})
+            losses_vector.append(float(loss))
+            epoch_vector.append(epoch)
+            if epoch%validation_epoch == 0:
+                predictions = self.infer_model(validation_data)
+                validation_accuracy.append(self.accuracy(predictions, validation_data['labels']))
+                validation_epoch_time.append(epoch)
+            epoch += 1
+
+        return epoch_vector, losses_vector, validation_epoch_time, validation_accuracy
+
+    def get_batch(self, data):
+        batch_image = []
+        batch_label = []
+        for image, label in zip(data['images'], data['labels']):
+            batch_image.append([np.reshape(image, (self.input_shape[0], self.input_shape[1], self.input_shape[2]))])
+            batch_label.append([np.reshape(label, self.num_output)])
+
+        return batch_image, batch_label
+
+    def accuracy(self, predictions, labels):
+        num_correct = 0
+        num_total = len(labels)
+        for prediction, label in zip(predictions, labels):
+            if np.argmax(prediction) == np.argmax(label):
+                num_correct += 1
+        return num_correct/num_total
+
+    def infer_model(self, data):
+        batch_images, batch_labels = self.get_batch(data)
+        predictions = []
+        for image, label in zip(batch_images, batch_labels):
+             predictions.append(self.sess.run(self.net, feed_dict={self.inputs:image})[0])
+
+        return predictions
