8cd4dfd460c85bb0e55cf7cf66a053f609995239
zhengqixi <zhengqixi@github.com>
2017-02-19 23:50:13 -0500

    Oh Dio, respondi

--- Assignment3/mnist_train.py
+++ Assignment3/mnist_train.py
@@ -1,3 +1,12 @@
-import  poopoonet
+from poopoonet import  poopoonet
+import tensorflow as tf
+from tensorflow.contrib.learn.python.learn.datasets.mnist import read_data_sets
 
-poopoonet.build_net()
+mnist = read_data_sets("mnist_data/",  one_hot=True)
+train = mnist[0]
+validate = mnist[1]
+test = mnist[2]
+dev = tf.constant(train.images, dtype=tf.float32, shape=[55000, 28, 28, 1])
+print(dev)
+net = poopoonet.build_model(dev, 10, True)
+print(net)
--- Assignment3/poopoonet.py
+++ Assignment3/poopoonet.py
@@ -5,23 +5,22 @@ class poopoonet:
     @staticmethod
     def build_model(input, num_output, training):
         with slim.arg_scope([slim.conv2d, slim.fully_connected],
-                            activation_fn=tf.nn.relu,
                             weights_initializer=tf.contrib.layers.xavier_initializer(),
                             weights_regularizer=slim.l2_regularizer(0.5)):
-            net = slim.layers.conv2d(input, 20, 3, scope='conv1')
+            net = slim.layers.convolution(input, num_outputs=20, kernel_size=3, scope='conv1')
             net = slim.layers.max_pool2d(net, 3, scope='pool1')
-            net = slim.layers.conv2d(net, 15, stride=1,scope='conv2')
+            net = slim.layers.convolution(net, num_outputs=15, kernel_size= 2, stride=1,scope='conv2')
             net = slim.layers.max_pool2d(net, 2, stride=2, scope='pool2')
             net = slim.layers.flatten(net, scope='flat')
-            net = slim.stack(net, slim.fully_connected, [150, 100 ], scope='fc')
-            net = slim.layers.dropout(net, keep_prob=0.75, is_training=training)
+            net = slim.layers.fully_connected(net, 100)
             net = slim.stack(net, slim.fully_connected, [50, num_output], scope='fc')
             return net
 
     @staticmethod
-    def train_model(images_train, labels_train, images_validate, labels_validate, outdir, num_output):
-        predictions =  poopoonet.build_model(images_train, num_output, False)
-        slim.losses.softmax_cross_entropy(predictions, labels_train)
+    def train_model(train_data, validate_data, outdir, num_output):
+        predictions =  poopoonet.build_model(train_data['images'], num_output, False)
+        slim.losses.softmax_cross_entropy(predictions, train_data['labels'])
         total_loss = slim.losses.get_total_loss()
         optimizer = tf.train.GradientDescentOptimizer()
-        trainer = slim.learning.create_train_op(total_loss, optimizer )
+        trainer = slim.learning.create_train_op(total_loss, optimizer)
+        print(trainer)
